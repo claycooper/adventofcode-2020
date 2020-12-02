e7f384e568d883f006cc747b03b0c04a4b477abd
Clay Cooper <cacits@rit.edu>
2020-12-01 21:58:33 -0500

    Day 1 Part 2 Complete

---
+++ day-01/day-01-part2.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+  "os"
+  "bufio"
+  "fmt"
+  "log"
+  "strconv"
+)
+
+
+func main() {
+  var expenses []int64
+
+  scanner := bufio.NewScanner(os.Stdin)
+  
+  for scanner.Scan() {
+    i, _ := strconv.ParseInt(scanner.Text(), 0, 64)
+    expenses = append(expenses, i) 
+  }
+  
+  if err := scanner.Err(); err != nil {
+    log.Println(err)
+  }
+
+  //fmt.Print(expenses)
+
+  for _, outerval := range expenses {
+    for _, innerval := range expenses {
+      for _, ternval := range expenses {
+        if outerval + innerval + ternval == 2020 {
+          fmt.Println(outerval * innerval * ternval )
+        }
+      }
+    }
+  }
+}
